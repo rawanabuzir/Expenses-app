900a1ea8293afc14d0ffb636c89d07e942ffcace
rawan_abuzir <rawanabuzerr@gmail.com>
2025-04-23 17:01:32 +0300

    Initial commit

---
+++ constants/styles.js
@@ -0,0 +1,16 @@
+export const GlobalStyles = {
+  colors: {
+    primary50: '#352D49FF',
+    primary100: '#7153B8FF',
+    primary200: '#a281f0',
+    primary400: '#D1C5EBFF',
+    primary500: '#5F547BFF',
+    primary700: '#D5C3E5FF',
+    primary800: '#D1CAE0FF',
+    accent500: '#FFFFFFFF',
+    error50: '#fcc4e4',
+    error500: '#9b095c',
+    gray500: '#39324a',
+    gray700: '#B5AFC2FF',
+  },
+};
---
+++ screens/ManageExpense.js
@@ -0,0 +1,82 @@
+import { useLayoutEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import Button from '../components/UI/Button';
+import IconButton from '../components/UI/IconButton';
+import { GlobalStyles } from '../constants/styles';
+
+function ManageExpense({ route, navigation }) {
+
+  const editedExpenseId = route.params?.expenseId;
+  let isEditing;
+  if (editedExpenseId && editedExpenseId !== 'null') {
+    isEditing = true;
+  } else {
+    isEditing = false;
+  }
+
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? 'Edit Expense' : 'Add Expense',
+    });
+  }, [navigation, isEditing]);
+
+  function deleteExpenseHandler() {}
+
+  function cancelHandler() {
+    navigation.goBack();
+    console.log('cancelHandler');
+  }
+
+  function confirmHandler() {}
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? 'Update' : 'Add'}
+        </Button>
+      </View>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
+}
+
+export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center',
+  },
+});
